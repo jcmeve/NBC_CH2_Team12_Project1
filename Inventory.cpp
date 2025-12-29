82b33a9e8e308455c7b48d67efabd7204ae6957d
허태린 <ouranos0902@gmail.com>
2025-12-29 20:16:07 +0900

    update
    아이템 추가, 아이템 제거

--- Inventory.cpp
+++ Inventory.cpp
@@ -1,1 +1,13 @@
 ﻿#include "Inventory.h"
+
+
+	void Inventory::addItem(Item* item) 
+	{
+		items.push_back(item);
+	}
+
+	void Inventory::removeItem(Item* item) 
+	{
+		items.erase(std::remove(items.begin(), items.end(), item), items.end());
+	}
+
--- Inventory.h
+++ Inventory.h
@@ -1,4 +1,16 @@
 ﻿#pragma once
-class Inventory {
+#include <vector>
+#include "Item.h"
+
+class Inventory
+{
+private:
+	std::vector<Item*> items;
+
+public:
+
+	void removeItem(Item* item);
+
+	void addItem(Item* item);
 };
 
