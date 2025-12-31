5b2fc89297219f0cb992f99cba552e97e4a168f4
jcm <jcmeve@naver.com>
2026-01-01 03:10:04 +0900

    W A

--- SoundManager.h
+++ SoundManager.h
@@ -1,4 +1,6 @@
 ﻿#pragma once
+#include <string>
+#include <unordered_set>
 class SoundManager {
 private:
     SoundManager();
@@ -8,6 +10,15 @@ private:
     SoundManager& operator=(const SoundManager&& other) = delete;
     SoundManager(const SoundManager&& other) = delete;
     friend class GameManager;
+
+    std::unordered_set<std::wstring> audioFiles;
+public:
+    void LoadAudio(const std::wstring& name);
+    void PlayAudio(const std::wstring& name, bool loop = false);
+    void StopAudio(const std::wstring& name);
+    void CloseAudio(const std::wstring& name);
+    void SetVolume(const std::wstring& name, int volume);
+
 
 };
 
