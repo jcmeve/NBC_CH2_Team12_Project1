00e50f3b59dc3b2a1e449ed9c0eb06a72d464fd4
Yujin Jang <89734178+yujinway@users.noreply.github.com>
2026-01-02 16:11:05 +0900

    Update Logger.cpp

--- Logger.cpp
+++ Logger.cpp
@@ -5,14 +5,14 @@ Logger::Logger() {
 }
 
 void Logger::ErrorLog(const std::wstring& s) const {
-    GM::GetDisplay().WriteString(s);
-    exit(-1);
+	GM::GetDisplay().WriteString(s);
+	exit(-1);
 }
 
 void Logger::Log(const std::wstring& s) const {
-    GM::GetDisplay().WriteString(s);
+	GM::GetDisplay().WriteString(s);
 }
 
 void Logger::LogSlow(const std::wstring& s, float time) const {
-    GM::GetDisplay().WriteStringSlow(s,time)
+	GM::GetDisplay().WriteStringSlow(s, time);
 }
