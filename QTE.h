22fd4a155a434119b62f7e3d2a7cd8fdb66a56af
Yujin Jang <89734178+yujinway@users.noreply.github.com>
2026-01-06 15:35:46 +0900

    Update QTE.h

--- QTE.h
+++ QTE.h
@@ -4,16 +4,19 @@
 class Character;
 class QTE : public Actor {
 private:
-    float limitTime;
-    float currTime;
-    Character* qteTarget;
-    bool fail = false;
-    bool isEnd = false;
+	float limitTime;
+	float currTime;
+	Character* qteTarget;
+	bool fail = false;
+	bool isEnd = false;
 public:
-    QTE(const std::wstring& name);
-    // Actor을(를) 통해 상속됨
-    void Tick(float deltaTime) override;
-    void Init(Character* target, float time);
-    //Character포인터 직접 가지고 있는데 이벤트로 변경해야함
+	QTE(const std::wstring& name);
+	// Actor을(를) 통해 상속됨
+	void Tick(float deltaTime) override;
+	void Init(Character* target, float time);
+	//Character포인터 직접 가지고 있는데 이벤트로 변경해야함
+
+	bool IsFinished() const { return isEnd; }
+	bool IsFailed() const { return fail; }
 };
 
